a1b62504615414b698af9a880ad67a23d9f0362b
gozixq <natsinee2545@gmail.com>
2021-11-30 17:33:51 +0700

    ppp

--- lab7_3.cpp
+++ lab7_3.cpp
@@ -1,15 +1,61 @@
 #include<iostream>
-
+#include<cmath>
 using namespace std;
+int adiff(int A,int B)
+{
+  int C,i=0;
+  while (i == 0)
+  {
+    if(A>360)
+    {
+      A -= 360;
+    }
+    else
+    {
+      i++;
+    }
+  }
+  i=0;
 
+  while (i == 0)
+  {
+    if(B>=360)
+    {
+      B -= 360;
+    }
+    else
+    {
+      i++;
+    }
+    
+  }
+  x=A-B;
+  y=(360-B)+A;
+  if(x<0)
+  {
+    x=x*x-1;
+  }
+  if(y<0)
+  {
+    y=y*-1;
+  }
+  if(x <= y)
+  {
+    return x;
+  }
+  else
+  {
+    return y;
+  }
+}
 
 int main(){
-  cout << adiff(180,270);
-  cout << adiff(210,45);
-  cout << adiff(0,360);
-  cout << adiff(10,350);
-  cout << adiff(95,260);
-  cout << adiff(90,-90);
-  cout << adiff(1000,280);
-  cout << adiff(60,244);
+  cout << adiff(180,270) << endl;
+  cout << adiff(210,45) << endl;
+  cout << adiff(0,360) << endl;
+  cout << adiff(10,350) << endl;
+  cout << adiff(95,260) << endl;
+  cout << adiff(90,-90) << endl;
+  cout << adiff(1000,280) << endl;
+  cout << adiff(60,244) << endl;
 }
